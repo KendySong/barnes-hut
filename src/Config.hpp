10ddedd31aa9bef4da40828e7c694464ee0e2d1a
Kendy Song <kendy.song@solmani.ch>
2023-06-23 14:14:01 +0200

    Compute the size of the root quad

--- src/Config.hpp
+++ src/Config.hpp
@@ -1,7 +1,13 @@
 #pragma once
 #include <cstdint>
+#include <SFML/Graphics.hpp>
 
 const std::uint32_t WIDTH = 1280;
 const std::uint32_t HEIGHT = 720;
 
 const std::uint32_t PLANETS_SPAWN = 100;
+const float GRAVITY = 500;
+const float MAX_FORCE = 0.01;
+
+const sf::Vector2f BORDER_OFFSET = sf::Vector2f(10, 10);
+const sf::Vector2f BORDER_OFFSET_SIZE = BORDER_OFFSET + BORDER_OFFSET;
--- src/Core/Sandbox.cpp
+++ src/Core/Sandbox.cpp
@@ -13,8 +13,8 @@
 Sandbox::Sandbox(sf::RenderWindow* window)
 {
 	p_window = window;
-	m_gravity = 500;
-	m_maxForce = 0.01;
+	m_gravity = GRAVITY;
+	m_maxForce = MAX_FORCE;
 	m_useBarneHut = true;
 
 	m_camera = Camera(500, sf::Vector2f(WIDTH / 2, HEIGHT / 2));
@@ -36,58 +36,30 @@ void Sandbox::update(float deltaTime)
 
 	if (m_useBarneHut)
 	{
-		//Compute limits of the root node
 		m_globalRoot.position = m_planets[0].body[0].position;
-		m_globalRoot.size = m_planets[0].body[0].position;
+		m_globalRoot.size = sf::Vector2f(1, 1);
+
+		//Construct the quad that contains all particules by getting the limits
 		for (auto& planet : m_planets)
 		{
 			const sf::Vector2f& position = planet.body[0].position;
-			const sf::Vector2f& size = planet.body[0].position;
 
 			m_globalRoot.position.x = m_globalRoot.position.x > position.x ? position.x : m_globalRoot.position.x;
 			m_globalRoot.position.y = m_globalRoot.position.y > position.y ? position.y : m_globalRoot.position.y;
 
-			m_globalRoot.size.x = m_globalRoot.size.x < position.x + size.x ? position.x + size.x : m_globalRoot.size.x;
-			m_globalRoot.size.y = m_globalRoot.size.y < position.y + size.y ? position.y + size.y : m_globalRoot.size.y;
+			m_globalRoot.size.x = m_globalRoot.size.x < position.x ? position.x : m_globalRoot.size.x;
+			m_globalRoot.size.y = m_globalRoot.size.y < position.y ? position.y : m_globalRoot.size.y;	
 		}
 
-		//Compute real size
-		m_globalRoot.size.x = m_globalRoot.size.x - m_globalRoot.position.x;
-		m_globalRoot.size.y = m_globalRoot.size.y - m_globalRoot.position.y;
+		//Compute the size of the box and add border
+		m_globalRoot.size = m_globalRoot.size - m_globalRoot.position;
+		m_globalRoot.position -= BORDER_OFFSET;
+		m_globalRoot.size += BORDER_OFFSET_SIZE;
 
-		//Transform quadtree border to square
-		m_globalRoot.size.x = m_globalRoot.size.x > m_globalRoot.size.y ? m_globalRoot.size.x : m_globalRoot.size.y;
-		m_globalRoot.size.y = m_globalRoot.size.y > m_globalRoot.size.x ? m_globalRoot.size.y : m_globalRoot.size.x;
+		//Set the quad to a square
+		m_globalRoot.size.x = m_globalRoot.size.x < m_globalRoot.size.y ? m_globalRoot.size.y : m_globalRoot.size.x;
+		m_globalRoot.size.y = m_globalRoot.size.y < m_globalRoot.size.x ? m_globalRoot.size.x : m_globalRoot.size.x;
 		m_globalRoot.construct();
-
-		//Generate the quadtree
-		m_root = Node(m_globalRoot);
-		VertexQuadTree::instance()->quads.clear();
-		for (auto& planet : m_planets)
-		{
-			m_root.insert(&planet);
-		}
-
-		/*
-		for (auto& planet : m_planets)
-		{
-			sf::Vector2f planetPos = planet.body[0].position;
-			for (auto& target : m_planets)
-			{
-				if (&planet == &target)
-				{
-					continue;
-				}
-
-				sf::Vector2f targetPos = target.body[0].position;
-				float force = (planet.mass * target.mass) / std::pow(Math::distance(planetPos, targetPos), 2) * m_gravity;
-				force = force > m_maxForce ? m_maxForce : force;
-				planet.velocity += Math::unit(targetPos - planetPos) * force;
-			}
-
-			planet.body[0].position = planetPos + planet.velocity * deltaTime;
-		}
-		*/
 	}
 	else
 	{
@@ -118,13 +90,7 @@ void Sandbox::draw() noexcept
 	
 	if (m_useBarneHut)
 	{
-		p_window->draw(m_root.quad.vertices);
-
-		const auto& quads = VertexQuadTree::instance()->quads;
-		for (size_t i = 0; i < quads.size(); i++)
-		{
-			p_window->draw(quads[i]);
-		}
+		p_window->draw(m_globalRoot.vertices);
 	}
 
 	for (auto& planet : m_planets)
